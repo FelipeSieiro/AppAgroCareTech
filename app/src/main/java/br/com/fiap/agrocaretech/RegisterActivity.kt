9992a8a9b8a8abe67c7cdeab79ed95408668605f
FelipeSieiro <felipedossantos0804@gmail.com>
2024-05-09 22:08:14 -0300

    feat: create initial page and register page

---
+++ app/src/main/java/br/com/fiap/agrocaretech/RegisterActivity.kt
@@ -0,0 +1,18 @@
+package br.com.fiap.agrocaretech
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+
+class RegisterActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+
+
+    }
+
+
+}
